13ae257c8452c160397a3b5b0aae38005dedc844
Roope <roopejuuz@gmail.com>
2023-09-27 19:25:14 +0300

    App updated
    Added REST service that returns one or all books (JSON). Enabled Spring Data REST.

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.example.Bookstore.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
@@ -20,11 +24,23 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 
-	@RequestMapping("/booklist")
+	@RequestMapping({"/","/booklist"})
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	// Rest all books http://localhost:8080/books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// Rest book by id http://localhost:8080/book/1
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }    
 		
 	// Add book
 	@RequestMapping(value = "/add")
--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,5 +1,7 @@
 package com.example.Bookstore.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +20,7 @@ public class Book {
 	private int publicationYear;
 	private double price;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "categoryid")
 	private Category category;
